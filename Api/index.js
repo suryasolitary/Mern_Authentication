a39c58f197f644a2d0f0c6a747add75f635aa738
surya <suryasurya1942004@gmail.com>
2024-01-09 21:03:22 +0530

    Database Connection

--- Api/index.js
+++ Api/index.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connection to MongoDb")
+}).catch((err)=>{
+    console.log(err)
+})
 const app = express()
 const port = 5000
 app.listen(port,()=>{
